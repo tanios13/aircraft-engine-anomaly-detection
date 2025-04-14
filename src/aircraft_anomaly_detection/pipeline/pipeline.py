5138a7c3383ca3a9988f0817ffcefc8300f12302
Shyngys Aitkainov <chikaphys9@gmail.com>
2025-04-14 19:12:38 +0200

    pipeline initialized

--- src/aircraft_anomaly_detection/pipeline/pipeline.py
+++ src/aircraft_anomaly_detection/pipeline/pipeline.py
@@ -1,6 +1,16 @@
-class Pipeline:
+import numpy as np
+from PIL import Image
+
+from ..interfaces import Prediction
+
+
+class AnomalyPipeline:
     def __init__(self):
         pass
 
-    def run(self):
-        pass
+    def run(self, image_input: str | Image.Image | np.ndarray) -> Prediction:
+        
+        raise NotImplementedError("The run method must be implemented in the subclass.")
+
+        
+        return Prediction()
