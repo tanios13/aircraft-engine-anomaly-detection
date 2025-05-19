86b5a8b802194087352e1e6fc93ac55a9cd97fb3
Daniyar Zakarin <daniyarzt@gmail.com>
2025-05-19 22:03:02 +0200

    Added image refinement that removes bboxes that are mostly in background.

--- src/aircraft_anomaly_detection/pipeline/evaluate.py
+++ src/aircraft_anomaly_detection/pipeline/evaluate.py
@@ -1,3 +1,7 @@
+from collections.abc import Callable
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import tqdm
 
@@ -8,10 +12,7 @@ from aircraft_anomaly_detection.viz_utils import visualize_mask_overlap_with_ima
 
 
 def evaluate(
-    dataset: AnomalyDataset,
-    model: ModelInterface,
-    output_dir: str = None,
-    CLIPBackgroundRemover: Callable | None = None,
+    dataset: AnomalyDataset, model: ModelInterface, output_dir: str = None, background_remover: Callable | None = None
 ):
     """
     Evaluate the model on the given dataset.
@@ -31,16 +32,21 @@ def evaluate(
         grd_annotation_list.append(metadata.annotation)
 
         # Remove background
-        if CLIPBackgroundRemover is not None:
-            image, background_mask = CLIPBackgroundRemover(image)
+        if background_remover is not None:
+            no_background_image, background_mask = background_remover(image)
         else:
-            background_mask = None
+            no_background_image, background_mask = image, None
 
         pred_annotation = model.predict(image)
+
+        # Postprocessing
+        if background_remover is not None:
+            refine_annotation(pred_annotation, background_mask)
+
         pred_annotation_list.append(pred_annotation)
 
         visualize_mask_overlap_with_image(
-            image,
+            no_background_image,
             metadata.annotation.mask,
             pred_annotation.mask,
             save_path=output_dir + f"image_{i}.png",
@@ -57,3 +63,26 @@ def evaluate(
     results_df.to_csv(output_dir + "results.csv", index=False)
 
     evaluator.plot_confusion_matrix(output_dir + "confusion_matrix.png")
+
+
+def refine_annotation(annotation, background_mask):
+    """
+    Removes rectangles with large background area.
+    """
+    valid_bbox_idx = []
+    for i in range(len(annotation.bboxes)):
+        x1, y1, x2, y2 = annotation.bboxes[i]
+        cropped = annotation.image.crop((x1, y1, x2, y2))
+        area = (x2 - x1) * (y2 - y1)
+        background_area = np.sum(background_mask[y1:y2, x1:x2])
+        if background_area / area < 0.5:
+            valid_bbox_idx.append(i)
+    print(f"Refinement: removed {len(annotation.bboxes) - len(valid_bbox_idx)} boxes from {len(annotation.bboxes)}")
+    annotation.bboxes = [annotation.bboxes[i] for i in valid_bbox_idx]
+    annotation.scores = [annotation.scores[i] for i in valid_bbox_idx]
+
+    if annotation.mask is not None:
+        annotation.mask[(background_mask == 1)] = 0
+
+    # TODO: add similar logic for mask annotations
+    annotation.damaged = len(annotation.bboxes) > 0
